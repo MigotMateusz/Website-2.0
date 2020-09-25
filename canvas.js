7bdf1707ae5149eaf1774c67e95653bcae8aee5a
Mateusz Migot <43936841+MigotMateusz@users.noreply.github.com>
2020-09-25 14:50:04 +0200

    added animated hello screen

---
+++ canvas.js
@@ -0,0 +1,116 @@
+/*var canvas = document.querySelector(".canvas")
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+addEventListener('resize', () => {
+  canvas.width = innerWidth
+  canvas.height = innerHeight
+
+  init()
+})
+
+class Particle {
+  constructor(x, y, radius) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.shadowColor = '#fff'
+    c.shadowBlur = 15
+    c.fillStyle = '#fff'
+    c.fill()
+    c.closePath()
+  }
+
+  update() {
+    this.draw()
+  }
+}
+
+let particles
+function init() {
+  particles = []
+
+  for (let i = 0; i < 1500; i++) {
+    const canvasWidth = canvas.width + 1000
+    const canvasHeight = canvas.height + 2000
+
+    const x = Math.random() * canvasWidth - canvasWidth / 2
+    const y = Math.random() * canvasHeight - canvasHeight / 2
+    const radius = 2 * Math.random()
+
+    particles.push(new Particle(x, y, radius))
+  }
+}
+
+// Animation Loop
+let radians = 0
+let alpha = 1
+function animate() {
+  requestAnimationFrame(animate)
+  c.fillStyle = `rgba(10, 10, 10, ${alpha})`
+  c.fillRect(0, 0, canvas.width, canvas.height)
+
+  c.save()
+  c.translate(canvas.width / 2, canvas.height / 2)
+  c.rotate(radians)
+  particles.forEach((particle) => {
+    particle.update()
+  })
+  c.restore()
+
+  radians += 0.003
+
+}
+
+init()
+animate()*/
+
+
+// Initialising the canvas
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+
+// Setting the width and height of the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Setting up the letters
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+letters = letters.split('');
+
+// Setting up the columns
+var fontSize = 10,
+    columns = canvas.width / fontSize;
+
+var counter = columns;
+
+// Setting up the drops
+var drops = [];
+for (var i = 0; i < columns; i++) {
+  drops[i] = 1;
+}
+var initInterval;
+// Setting up the draw function
+function draw() {
+  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < drops.length; i++) {
+    var text = letters[Math.floor(Math.random() * letters.length)];
+    ctx.fillStyle = '#444';
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+    drops[i]++;
+    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+      drops[i] = 0;
+    }
+  }
+}
+
+initInterval = setInterval(draw, 33);
+
